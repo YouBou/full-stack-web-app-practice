107ea92fb8ed40370a8a9f902f3645106387fa1e
ubow <ikas2lite@gmail.com>
2024-09-19 20:31:12 +0900

    [feat] トークン再発行用のエンドポイント追加

--- serverside/api/inventory/views.py
+++ serverside/api/inventory/views.py
@@ -10,6 +10,7 @@ from api.inventory.exception import BusinessException
 from django.db.models.functions import Coalesce
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 
 
 class ProductView(APIView):
@@ -159,3 +160,22 @@ class LoginView(APIView):
             response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
             return response
         return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+class RetryView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = []
+
+    def post(self, request):
+        request.data['refresh'] = request.META.get('HTTP_REFRESH_TOKEN')
+        serializer = TokenRefreshSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        access = serializer.validated_data.get("access", None)
+        refresh = serializer.validated_data.get("refresh", None)
+        if access:
+            response = Response(status=status.HTTP_200_OK)
+            max_age = settings.COOKIE_TIME
+            response.set_cookie('access', access, httponly=True, max_age=max_age)
+            response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
+            return response
+        return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
+    
